80ba5c577a8c155904fd0a81388d16fd6a4c4bbe
Przemek <pdec86dev@gmail.com>
2024-02-15 11:04:17 +0100

    Handle assets building

--- DockerPHP/scripts/init-php.sh
+++ DockerPHP/scripts/init-php.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd /var/www/html/
-composer install -n
+composer install --no-dev --optimize-autoloader
 
 # bin/console secrets:list --reveal
 cat $APP_SECRET_FILE | bin/console secrets:set APP_SECRET - && cat $APP_SECRET_FILE | bin/console secrets:set APP_SECRET - --local
@@ -11,6 +11,7 @@ cat $DB_PASS_FILE | bin/console secrets:set DATABASE_PASS - && cat $DB_PASS_FILE
 cat $MESSENGER_TRANSPORT_DSN_FILE | bin/console secrets:set MESSENGER_TRANSPORT_DSN - && cat $MESSENGER_TRANSPORT_DSN_FILE | bin/console secrets:set MESSENGER_TRANSPORT_DSN - --local
 # bin/console secrets:decrypt-to-local --force
 
+APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 bin/console doctrine:migrations:migrate
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/sf_messenger.conf
